96da897b9490cbcae678adc2e70f8b02aabb5f60
Morgan Frodsham <morgan@frodsham.net>
2021-11-25 21:55:48 +0000

    Transformed FL_data1 so values are TRUE/FALSE, investigated data with count and ggplot, created full archetype dataset and joined full enrolments with full archetype datasets.

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,31 +1,26 @@
-# Import enrolments data for cyber security course
-cyber_security_1_enrolments <- read_csv("data/cyber-security-1_enrolments.csv")
-cyber_security_2_enrolments <- read_csv("data/cyber-security-2_enrolments.csv")
-cyber_security_3_enrolments <- read_csv("data/cyber-security-3_enrolments.csv")
-cyber_security_4_enrolments <- read_csv("data/cyber-security-4_enrolments.csv")
-cyber_security_5_enrolments <- read_csv("data/cyber-security-5_enrolments.csv")
-cyber_security_6_enrolments <- read_csv("data/cyber-security-6_enrolments.csv")
-cyber_security_7_enrolments <- read_csv("data/cyber-security-7_enrolments.csv")
+# Combine enrolments data for cs course into cyber_security_full_enrolments
+cyber_security_full_enrolments <- rbind(cyber.security.1_enrolments, cyber.security.2_enrolments, cyber.security.3_enrolments, cyber.security.4_enrolments, cyber.security.5_enrolments, cyber.security.6_enrolments, cyber.security.7_enrolments)
 
-#Combine enrolments data for cs course into cyber_security_full_enrolments
-cyber_security_full_enrolments <- rbind(cyber_security_1_enrolments, cyber_security_2_enrolments, cyber_security_3_enrolments, cyber_security_4_enrolments, cyber_security_5_enrolments, cyber_security_6_enrolments, cyber_security_7_enrolments)
+# Create data set of students and demographic data for initial investigation
+FL_data1 <- as_tibble(cyber_security_full_enrolments) %>% #Create tibble
+  mutate(completed = !is.na(fully_participated_at), #Is there a non-NA value in fully_participated_at?
+         gender_declared = gender != "Unknown", #Is gender something other than unknown?
+         country_declared = country != "Unknown", #Is country something other than unknown?
+         age_declared = age_range != "Unknown", #Is age something other than unknown?
+         education_declared = highest_education_level != "Unknown", #Is education level something other than unknown?
+         employment_status_declared = employment_status != "Unknown", #Is employment status something other than unknown?
+         employment_area_declared = employment_area != "Unknown") #Is employment area something other than unknown?
 
-#Create data set of students and demographic data for initial investigation
-FL_data1 <- as_tibble(cyber_security_full_enrolments)
-FL_data1[is.na(FL_data1)] <- "Incomplete"
-print(FL_data1, n = 10, width = Inf)
-FL_data1 %>%
-  select(fully_participated_at, gender, country, age_range, highest_education_level, employment_status, employment_area) %>%
+#Combine archetype data for cs course into cyber_security_full_archetypes
+cyber_security_full_archetypes <- rbind(cyber.security.1_archetype.survey.responses, cyber.security.2_archetype.survey.responses, cyber.security.3_archetype.survey.responses, cyber.security.4_archetype.survey.responses, cyber.security.5_archetype.survey.responses, cyber.security.6_archetype.survey.responses, cyber.security.7_archetype.survey.responses)
 
-  
+# Join between enrolments and archetype_survey_response on learner_id
+# More info on joins: https://dplyr.tidyverse.org/reference/mutate-joins.html 
+left_join(cyber_security_full_enrolments, cyber_security_full_archetypes, by = "learner_id", archetype)
 
-#Group students who have completed the course
+
+#Filter students who have completed the course
 course_completed <- filter(cyber_security_full_enrolments, !is.na(cyber_security_full_enrolments$fully_participated_at))
-course_completed <- as_tibble(course_completed)
-course_completed %>%
-  select(fully_participated_at, gender, country, age_range, highest_education_level, employment_status, employment_area) %>%
-  
-print(course_completed, n = 10, width = Inf)
 
-#Group students who have not completed the course
+#Filter students who have not completed the course
 course_incomplete <- filter(cyber_security_full_enrolments, is.na(cyber_security_full_enrolments$fully_participated_at))
